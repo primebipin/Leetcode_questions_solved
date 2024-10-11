c9c54eacbfe1ea0ed77c19b322e5ec225ffc92e2
Bipin kumar <112895327+primebipin@users.noreply.github.com>
2024-10-11 06:08:42 +0530

    Time: 55 ms (42.31%) | Memory: 47 MB (25.09%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        stack<int> st;
+
+        for(int i=0;i<nums.size();i++){
+            if(st.empty() || nums[st.top()]>nums[i]){
+                st.push(i);
+            }
+        }
+        int ans=0;
+        for(int i=nums.size()-1;i>ans;i--){
+            while(!st.empty() && nums[st.top()]<=nums[i]){
+                ans=max(ans,i-st.top());
+                st.pop();
+            }
+        }
+
+        return ans;
+    }
+};
